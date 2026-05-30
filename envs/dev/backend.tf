79b206d216b5874d340308cbe94804bd27380b22
Rajesh <sebrajesh@gmail.com>
2026-05-30 19:08:08 +1000

    chor: merge upstream latest

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,8 +2,9 @@
 
 terraform {
   backend "s3" {
-    bucket = "zen-pharma-terraform-state-dpp-2026"
-    key    = "envs/dev/terraform.tfstate"
+    bucket       = "zen-pharma-terraform-state-sebrajesh"  # Replace with your S3 bucket name
+    key          = "envs/dev/terraform.tfstate"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true   # S3 native locking
   }
--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "zen-pharma-terraform-state-dpp-2026"
-    key    = "envs/prod/terraform.tfstate"
+    bucket       = "zen-pharma-terraform-state-sebrajesh"  # Replace with your S3 bucket name
+    key          = "envs/prod/terraform.tfstate"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true   # S3 native locking — requires Terraform 1.10+, no DynamoDB needed
   }
